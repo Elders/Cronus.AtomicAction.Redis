c22e83bf998d7e09bcabd4b95c5dbdfff2ba6367
mynkow <mynkow@gmail.com>
2018-10-23 10:17:05 +0300

    Adds configurations for the Redlock

--- src/Elders.Cronus.AtomicAction.Redis.Tests/TestAtomicActionFactory.cs
+++ src/Elders.Cronus.AtomicAction.Redis.Tests/TestAtomicActionFactory.cs
@@ -1,4 +1,5 @@
-﻿using Elders.Cronus.AtomicAction.Redis.RevisionStore;
+﻿using Elders.Cronus.AtomicAction.Redis.Config;
+using Elders.Cronus.AtomicAction.Redis.RevisionStore;
 
 namespace Elders.Cronus.AtomicAction.Redis.Tests
 {
@@ -6,7 +7,7 @@ namespace Elders.Cronus.AtomicAction.Redis.Tests
     {
         public static IAggregateRootAtomicAction New(ILock aggregateRootLock, IRevisionStore revisionStore)
         {
-            return new RedisAggregateRootAtomicAction(aggregateRootLock, revisionStore);
+            return new RedisAggregateRootAtomicAction(aggregateRootLock, revisionStore, RedisAtomicActionOptions.Defaults);
         }
     }
 }
--- src/Elders.Cronus.AtomicAction.Redis/Config/RedisAtomicActionOptions.cs
+++ src/Elders.Cronus.AtomicAction.Redis/Config/RedisAtomicActionOptions.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 
 namespace Elders.Cronus.AtomicAction.Redis.Config
 {
     public class RedisAtomicActionOptions
     {
-        private static RedisAtomicActionOptions defaults = new RedisAtomicActionOptions
+        public RedisAtomicActionOptions(IConfiguration configuration)
+        {
+            LockTtl = GetValue(configuration, "cronus_atomicaction_redis_ttl_lock_ms", TimeSpan.FromMilliseconds(1000));
+            ShorTtl = GetValue(configuration, "cronus_atomicaction_redis_ttl_short_ms", TimeSpan.FromMilliseconds(1000));
+            LongTtl = GetValue(configuration, "cronus_atomicaction_redis_ttl_long_ms", TimeSpan.FromMilliseconds(300000));
+        }
+
+        RedisAtomicActionOptions() { }
+
+        private static RedisAtomicActionOptions defaults = new RedisAtomicActionOptions()
         {
             LockTtl = TimeSpan.FromSeconds(1),
             ShorTtl = TimeSpan.FromSeconds(1),
@@ -18,5 +28,13 @@ namespace Elders.Cronus.AtomicAction.Redis.Config
         public TimeSpan LongTtl { get; set; }
 
         public static RedisAtomicActionOptions Defaults { get { return defaults; } }
+
+        TimeSpan GetValue(IConfiguration configuration, string key, TimeSpan defaultValue)
+        {
+            var value = configuration[key];
+            if (string.IsNullOrEmpty(value)) return defaultValue;
+
+            return TimeSpan.FromMilliseconds(double.Parse(value));
+        }
     }
 }
